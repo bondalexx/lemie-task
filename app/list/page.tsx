59332c89364e79a4328d4243007d4fb49b2eb24e
Alex Bondarenko <bond.alexx@gmail.com>
2025-04-17 19:43:25 +0300

    list change

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,9 +1,10 @@
+import { Suspense } from "react";
 import MemeList from "@/components/MemeList";
 
 export default function ListPage() {
   return (
-    <div>
+    <Suspense fallback={<div>Loading memes...</div>}>
       <MemeList />
-    </div>
+    </Suspense>
   );
 }
